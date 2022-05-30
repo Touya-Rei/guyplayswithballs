bf727c5f0b3b7987ceaa76fc6ec612e1b4133097
Touya-Rei <106257286+Touya-Rei@users.noreply.github.com>
2022-05-30 10:04:56 -0400

    set boolean for future usage
    built the foundation for rng on/off switches every reset

--- Switch.java
+++ Switch.java
@@ -3,7 +3,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Switch extends Actor
 {
-        
+    int min;
+    int max;
+    boolean off= false;
+    boolean on= false;
+    
+    
+    
     public void act()
     {
         
