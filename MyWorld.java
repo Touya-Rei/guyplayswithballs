5b319dad3bf32d7cb4a566349adc1ec1603cb573
Touya-Rei <106257286+Touya-Rei@users.noreply.github.com>
2022-05-30 10:14:21 -0400

    downloaded and cropped on/oof img
    testing setting  images based off of boolean

--- MyWorld.java
+++ MyWorld.java
@@ -10,9 +10,15 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-
         super(600, 400, 1); 
         
+        
+        for (int i= 0;i<=7;i++)
+        {
+            int x= 100+i*55;
+            int y= 200;
+            addObject( new Switch(),x, y);
+        }    
         
     }
 }
