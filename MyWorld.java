0701c5748739b529b377f2c71d147040704a8d23
Touya-Rei <106257286+Touya-Rei@users.noreply.github.com>
2022-05-30 11:10:44 -0400

    laid foundation for tommorrows  changes with rng

--- MyWorld.java
+++ MyWorld.java
@@ -3,33 +3,26 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class MyWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
+        Switch[] switches;
     public MyWorld()
     {    
         super(600, 400, 1); 
-<<<<<<< Updated upstream
         
+        switches= new Switch[7];
+    
         
-        for (int i= 0;i<=7;i++)
+        for (int i= 0;i<switches.length;i++)
         {
             int x= 100+i*55;
             int y= 200;
-            addObject( new Switch(),x, y);
+            int random=  Greenfoot.getRandomNumber(2);
+            switches[i]= new Switch();
+            addObject(switches[i],x, y);
         }    
         
-=======
-        int random=  Greenfoot.getRandomNumber(2);
+
         
-        for (int i= 0;i<=7;i++)
-        {
-            int x = 100+i*55;
-            int y = 200;
-            addObject( new Switch(),x, y);
-        }        
->>>>>>> Stashed changes
+            
+
     }
 }
--- Switch.java
+++ Switch.java
@@ -3,17 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Switch extends Actor
 {
-<<<<<<< Updated upstream
-    int min;
-    int max;
     boolean off= false;
     boolean on= false;
-    
-    
-    
-=======
-    boolean on= false;
->>>>>>> Stashed changes
     public void act()
     {
         
