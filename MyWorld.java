5e0b3fe7b6ca191bc5788f42a517e8b5b127f8ed
Touya-Rei <106257286+Touya-Rei@users.noreply.github.com>
2022-06-06 09:04:02 -0400

    Finished the project with the win con added

--- MyWorld.java
+++ MyWorld.java
@@ -3,16 +3,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class MyWorld extends World
 {
+    //setting up the array
     Switch[] switches;
+    //setting up the arrow to be recognized
     Arrow arrow;
+    //selection allows us to track which switch are we highlighting or in this case pointing at
     int selection =0;
-    int  ind;
+    //if you don't know what this is I don't even know
+    int howmanyswitchesareon=3;
+    int initialvalue=0;
     
     public MyWorld()
     {    
         super(600, 400, 1); 
         
-        switches= new Switch[7];
+        switches= new Switch[8];
         arrow= new Arrow();
 
         
@@ -25,10 +30,10 @@ public class MyWorld extends World
 
         
         
-        
+        //common array spaw set up, we use switches.length to mark the threshhold, and the for loop makes sure we hit that threshhold
         for (int i= 0;i<switches.length;i++)
         {
-            int x= 100+i*70;
+            int x= 50+i*70;
             int y= 200;
             int random=  Greenfoot.getRandomNumber(2);
             switches[i]= new Switch();
@@ -37,8 +42,9 @@ public class MyWorld extends World
             addObject(switches[i],x, y);
             
             
-            
-            if (i%6==0)
+            //checks if the switches have been added yet since we need the full length because if we do this before checking
+            //greenfoot cries and says nullpointer exception
+            if (i%switches.length==0)
             {
                 addObject(arrow,100+switches.length, 50);
                 
@@ -48,8 +54,8 @@ public class MyWorld extends World
             
         }
 
-        
-        for(int m= 0;m<3;m++)
+        //we want 2 switches to be on so 
+        for(int m= 0;m<howmanyswitchesareon;m++)
             {
                 int randorder=  Greenfoot.getRandomNumber(6);
                 switches[randorder].on=true;
@@ -67,7 +73,8 @@ public class MyWorld extends World
         
         
         
-        
+        //basic movement but with a twist of referencing which element of the array are we currently selecting now, and moving the player to that specific x location
+        //the switcheslength-1 is there to safe guard the condition from a nullpointer exception or an array out of bounds
         if (arrow!=null&&Greenfoot.isKeyDown("d")&&selection<switches.length-1)
         {
             selection=selection+1;
@@ -86,7 +93,8 @@ public class MyWorld extends World
         }
         
         
-        
+        //selection tells us which element in the array we are talking about, the =! flips the values inside the actor itself, we use the selection+1and -1 for switching both sides
+        //and I added extra conditions as to when the player is flicking the switches at the ends.
         if(Greenfoot.isKeyDown("enter"))
         {
             switches[selection].on=!switches[selection].on;
@@ -103,11 +111,25 @@ public class MyWorld extends World
             {
                 switches[selection-1].on=!switches[selection].on;
             }
+            //checks if all onvalues which is worth 1 if on is equal to the lenght of th earray
+            //so it means it's checking if all values in the array are true
+            
+                initialvalue =initialvalue+ switches[switches.length-1].onvalue;
+                
+                if (initialvalue==switches.length)
+                {
+                    addObject(new YouWin(), 300, 200);
+                }
+
+            
         }
         
         
         
+        
+        }
+        
     }
     
     
-}
+
--- Switch.java
+++ Switch.java
@@ -4,15 +4,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Switch extends Actor
 {
     boolean on= false;
+    //we set false as 0 we're going to use this variable to add up and see if they equal wswitches lenght
+    int onvalue=0;
     public void act()
     {
         if(on==true)
         {
             setImage("lever/2.png");
+            onvalue=1;
         }
         if(on==false) 
         {
             setImage("lever/1.png");
+            onvalue=0;
         }
         
     }
